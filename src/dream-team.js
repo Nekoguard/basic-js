ded1cc3cdab52ce876f3617c8f20fea0127969b5
Nekoguard <nekoguard@protonmail.com>
2021-09-08 13:55:02 +0300

    feat: implement dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,18 +2,28 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(members) {
+  if (!members || Array.isArray(members) === false) return false;
+
+  let result = [];
+
+  members.forEach(member => {
+    if (typeof(member) === "string") {
+      member = member.trim();
+      result.push(member[0].toUpperCase());
+    }
+  })
+
+  return result.sort().join('');
 }
