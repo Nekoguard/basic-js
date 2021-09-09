93ec193280ebbc9a517080715ab0bed2fade81c2
Nekoguard <nekoguard@protonmail.com>
2021-09-09 13:13:40 +0300

    feat: implement transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,5 +14,49 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function transform(arr) {
-  throw new NotImplementedError('Not implemented');
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+
+  if (!arr.includes('--discard-next') && !arr.includes('--discard-prev') && !arr.includes('--double-next') && !arr.includes('--double-prev')) {
+    return arr;
+  }
+
+  let result = [];
+  let isDiscardNext = false;
+  let isDoubleNext = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (isDiscardNext) {
+      isDiscardNext = false;
+      continue;
+    }
+
+    let elem = arr[i];
+
+    if (Number.isInteger(elem)) {
+      if (isDoubleNext) {
+        result.push(elem);
+        isDoubleNext = false;
+      }
+
+      result.push(elem);
+
+    } else if (elem === '--discard-next') {
+      isDiscardNext = true;
+
+    } else if (elem === '--discard-prev' && i > 0) {
+      result.splice(i - 1, 1);
+
+    } else if (elem === '--double-next') {
+      isDoubleNext = true;
+
+    } else if (elem === '--double-prev') {
+      if (i > 0) {
+        result.push(arr[i - 1]);
+      }
+    }
+  }
+
+  return result;
 }
